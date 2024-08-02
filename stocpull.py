e2ce35ab20388c2633ec84346dff96aff3e51f7c
buğra uysal <uysalbugra134@gmail.com>
2024-08-02 04:17:54 +0300

    hisse aramada arama cubuğu hep yenileniyor her zaman hisse arayabileceksiniz
    yeni eklenecekler ana sayfaya direk görebileceğiniz hisse fiyatları kartları eklenecek

--- stocpull.py
+++ stocpull.py
@@ -1,25 +1,39 @@
 import yfinance as yf
 
+import tkinter as tk 
+from tkinter import ttk
 
 
+ff=tk.Tk()
+ff.geometry=("600x800")
 
-
-hh=input("hisse yaz")
-try:
    
+while True:
+    hh=input("hisse yazı")
+    if hh.lower()=="bitti":
+        print("bitti")
+        break
 
-   aaple=yf.Ticker(hh)
-   
-   hisse=aaple.history(period="1d")
-   if not hisse.empty:
-      
-      price=hisse['Close'].iloc[-1]
-      print(f"{hh} tekrar deneyin: {price:.2f} USD " )
-   else:
-      
-         print("yeniden deneyin")   
+    else:
+        print("girdiler")
+        
+        try:
+           
+        
+           aaple=yf.Ticker(hh)
+           
+           hisse=aaple.history(period="1d")
+           if not hisse.empty:
+              
+              price=hisse['Close'].iloc[-1]
+              print(f"{hh} tekrar deneyin: {price:.2f} USD " )
+           else:
+              
+                 print("yeniden deneyin")   
+        
+        
+        except Exception as e:
+            print(f"hisse: {str (e)}")
+        
 
-
-except Exception as e:
-    print(f"hisse: {str (e)}")
-
+ff.mainloop()
