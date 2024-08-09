167de072d8e135bc18649180ad4f9c1958252347
buğra uysal <uysalbugra134@gmail.com>
2024-08-09 04:11:12 +0300

    tradingview dan abd hisse verileri cekildi

--- stocpull.py
+++ stocpull.py
@@ -1,7 +1,9 @@
-
 import requests
 from bs4 import BeautifulSoup
+
+""" tr borsası """
 urlm = " https://www.getmidas.com/canli-borsa/"
+
     
 r = requests.get(urlm)
 g = r.content
@@ -14,5 +16,18 @@ ss = ff.find_all("td", class_="val")
 """ hisse ismi çekme """
 gzz=ff.find_all("a",class_="title stock-code" )
    
-for ısım,fıyat in zip(gzz, ss):
-    print(f"{ısım.text.strip()}={fıyat.text.strip()} ")
+
+
+""" abd borsası """
+
+tblurl="https://tr.tradingview.com/markets/stocks-usa/market-movers-all-stocks/"
+aa=requests.get(tblurl)
+bb=aa.content
+cc=BeautifulSoup(bb,"html.parser")
+""" isim """
+dd=cc.find_all('sup', class_="apply-common-tooltip tickerDescription-GrtoTeat")
+""" fiyat """
+ff=cc.find_all("td", class_="cell-RLhfr_y4 right-RLhfr_y4")
+""" sonuç cıkdılar """
+for ısım,fıyat, name, price in zip(gzz, ss,dd,ff):
+    print(f"{ısım.text.strip()}={fıyat.text.strip()} ---------- {name.text.strip()}={price.text.strip()}  ")
