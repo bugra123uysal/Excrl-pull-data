4cd1bc3125be671aa71b7f564ffa07d783c8f971
buğra uysal <uysalbugra134@gmail.com>
2024-08-12 04:31:49 +0300

    tradigview dem kripto para verileri çekildi

--- stocpull.py
+++ stocpull.py
@@ -16,7 +16,8 @@ ss = ff.find_all("td", class_="val")
 """ hisse ismi çekme """
 gzz=ff.find_all("a",class_="title stock-code" )
    
-
+for ısım,fıyat in zip(gzz, ss):
+    print(f"{ısım.text.strip()}={fıyat.text.strip()}   ")
 
 """ abd borsası """
 
@@ -29,5 +30,20 @@ dd=cc.find_all('sup', class_="apply-common-tooltip tickerDescription-GrtoTeat")
 """ fiyat """
 ff=cc.find_all("td", class_="cell-RLhfr_y4 right-RLhfr_y4")
 """ sonuç cıkdılar """
-for ısım,fıyat, name, price in zip(gzz, ss,dd,ff):
-    print(f"{ısım.text.strip()}={fıyat.text.strip()} ---------- {name.text.strip()}={price.text.strip()}  ")
+for  name, price in zip(dd,ff):
+    print(f" {name.text.strip()}={price.text.strip()}  ")
+
+
+""" kripyo  """
+krpturl="https://tr.tradingview.com/markets/cryptocurrencies/prices-all/"
+bır=requests.get(krpturl)
+ıkı=bır.content
+uc=BeautifulSoup(ıkı,"html.parser")
+
+""" isim """
+dort=uc.find_all("a", class_="apply-common-tooltip tickerNameBox-GrtoTeat tickerName-GrtoTeat" )
+""" fiyat """
+bes=uc.find_all("td", class_="cell-RLhfr_y4 right-RLhfr_y4")
+
+for krptisim, krptpricee in zip(dort, bes):
+    print(f"{krptisim.text.strip()}= {krptpricee.text.strip()}")
